9a645366294b912220997b765525ce7422c523a2
oluwayemisiismail <ismailoluwayemisi@gmail.com>
2023-01-04 15:53:36 +0100

    DSA

---
+++ Array_List/longest_common_prefix.py
@@ -0,0 +1,14 @@
+
+
+def prefix(lst):
+    total = ""
+    get = []
+    final = []
+    # for i in lst:
+    #     if i
+    [i for i in lst if sub in i]
+
+
+
+lst = ["flower", "flow", "flight"]
+print(prefix(lst))
